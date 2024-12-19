fbe6e7a728e209ecaeb8b037223bcad9624f470d
khsei <khsei@naver.com>
2024-12-19 11:26:22 +0900

    쿼리스트링을 이용한 URL 파라미터 받아오기

--- src/main/java/com/sbs/tutorial1/controller/HomeController.java
+++ src/main/java/com/sbs/tutorial1/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.sbs.tutorial1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -38,5 +39,10 @@ public class HomeController {
         return num;
     }
 
+    @GetMapping("home/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam int b) {
+        return a + b;
+    }
 
 }
